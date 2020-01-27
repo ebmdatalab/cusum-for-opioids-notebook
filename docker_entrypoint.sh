c6cfa561512ab51f4b774b04c5138a5536d63ac5
Seb Bacon <seb.bacon@gmail.com>
2020-01-27 15:25:16 +0000

    Try a hack around a binder config issue

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 DIR=$1
+# This script is invoked as `docker_entrypoint.sh <PYTHONPATH> jupyter-notebook --opt1, opt2...
+# or as `docker_entrypoint.sh <PYTHONPATH>  --opt1, opt2...
 shift 1
+if [ "$1" == "jupyter-notebook" ]; then
+    shift 1
+fi
 cd $DIR
-exec sh -c "PYTHONPATH=$DIR jupyter lab --config=config/jupyter_notebook_config.py $@"
+exec sh -c "PYTHONPATH=$DIR jupyter lab --debug  $@"
